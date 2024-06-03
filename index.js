ed71083a7e8b2d833c74ad700ec381106246bae5
ellen <ellenjojeffries@gmail.com>
2024-06-02 18:39:39 -0600

    index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function divide(someValue){
+    let a = 2000
+    let b = 100
+return someValue = a/b
+}
+
+const square = parameter1 => parameter1 * parameter1
+const add = (par2, par3) => par2 + par3
